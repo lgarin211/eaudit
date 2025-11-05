84e157232d345cfad8f4d6bc2ab85d706c97e406
Agustinus <57866141+lgarin211@users.noreply.github.com>
2025-11-05 16:32:42 +0700

    Revert "re integrate"
    This reverts commit 14ae2f17f5629ab0bae856b1b75b35f0c08a8404.

--- resources/views/PemeriksaTL/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('template')
-@section('content')
-    <div class="container">
-        <p style="margin-left: 10px; margin-top:10px; color:black"> Hai <b>{{ session('name') }}  </b> dengan <b>{{ session('username') }}</b> serta <i>{{ session('level') }}</i></p>
-    </div>
-@endsection
